a37ed2d38e656d7ba78cb3fae2afe9347e041df4
Djonatan Arns <djonatanarns28@gmail.com>
2024-11-14 16:28:27 -0300

    Inicial commit, project setup

---
+++ src/main/java/com/github/djoarns/payflow/payflowmanager/PayflowManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.djoarns.payflow.payflowmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayflowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayflowManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/djoarns/payflow/payflowmanager/PayflowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.djoarns.payflow.payflowmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayflowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
