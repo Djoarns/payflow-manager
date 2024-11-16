cea04b9edbf0fdf7ae448f72fc13928257902277
Djonatan Arns <djonatanarns28@gmail.com>
2024-11-16 18:09:55 -0300

    feat(security): implement UserDetailsImpl - Implement UserDetails interface for Spring Security integration - Adapt User entity to UserDetails contract - Provide user authentication and authorization information

---
+++ src/main/java/com/github/djoarns/payflow/infrastructure/security/user/UserDetailsImpl.java
@@ -0,0 +1,51 @@
+package com.github.djoarns.payflow.infrastructure.security.user;
+
+import com.github.djoarns.payflow.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private final transient User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword().getValue();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername().getValue();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
