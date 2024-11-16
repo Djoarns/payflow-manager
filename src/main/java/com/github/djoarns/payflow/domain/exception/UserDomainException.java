c9be135b33cf1d8f900de37f3f04cb5e1369fa95
Djonatan Arns <djonatanarns28@gmail.com>
2024-11-16 17:53:44 -0300

    feat(user): add base UserDomainException class - Define a base exception class for user-related domain exceptions - Extend DomainException for consistent exception hierarchy

---
+++ src/main/java/com/github/djoarns/payflow/domain/exception/UserDomainException.java
@@ -0,0 +1,7 @@
+package com.github.djoarns.payflow.domain.exception;
+
+public class UserDomainException extends DomainException {
+    public UserDomainException(String message) {
+        super(message);
+    }
+}
