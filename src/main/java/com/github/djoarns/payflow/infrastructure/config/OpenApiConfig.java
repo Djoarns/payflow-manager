45da433cb161723b8013b07a5e27c231ca17a053
Djonatan Arns <djonatanarns28@gmail.com>
2024-11-17 13:39:41 -0300

    feat(docs): add OpenAPI/Swagger configuration

---
+++ src/main/java/com/github/djoarns/payflow/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package com.github.djoarns.payflow.infrastructure.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "PayFlow API",
+                version = "1.0",
+                description = "API for managing bills and payments",
+                contact = @Contact(
+                        name = "PayFlow Team",
+                        email = "support@payflow.com",
+                        url = "https://github.com/djoarns/payflow"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "Development Server"
+                )
+        },
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
